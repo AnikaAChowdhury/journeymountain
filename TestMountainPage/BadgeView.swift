f254b7761d76dcb8ffcbecb5cc0ab7cb4c555d17
michael-mendel <mmendel2501@gmail.com>
2022-10-17 11:04:08 -0400

    added badges

---
+++ TestMountainPage/BadgeView.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeView.swift
+//  TestMountainPage
+//
+//  Created by Michael Mendel on 10/17/22.
+//
+
+import SwiftUI
+
+struct BadgeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeView()
+    }
+}
