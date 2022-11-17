dae22aae5f980c2ad91567fcde49c5662afca4d0
AnikaAChowdhury <chowdhury.a@ufl.edu>
2022-11-16 19:44:01 -0600

    Added messages send by Garrett

--- TestMountainPage/Views/PopUpMessage.swift
+++ TestMountainPage/Views/PopUpMessage.swift
@@ -2,7 +2,7 @@
 //  PopUpMessage.swift
 //  TestMountainPage
 //
-//  Created by Chowdhury,Anika on 10/12/22
+//  Created by Chowdhury,Anika and Michael Mendel
 
 import SwiftUI
 
@@ -15,7 +15,7 @@ struct PopUpMessage: View {
         var message = ""
         
         if(todaysProgressPercent < 50){
-            let randomInt = Int.random(in: 1..<12)
+            let randomInt = Int.random(in: 1..<15)
             
             switch(randomInt){
             case 1:
@@ -26,6 +26,7 @@ struct PopUpMessage: View {
                 message = "What cancer fighting colors did you miss today?"
                 
             //Category 1:  <40% in blue zone >60% in yellow red zone for case 4 to 8
+                
             case 4:
                 message = "Start planning your next meal and try to include 2 vegetables."
             case 5:
@@ -36,6 +37,12 @@ struct PopUpMessage: View {
                 message = "Start with 2-3  better choices each day. "
             case 8:
                 message = "Choose 2 fruits and 2 vegetables that you’ll make!"
+            case 9:
+                message = "BEWARE! Fueling inflammation!"
+            case 10:
+                message = "What better choices can you make for your next meal?"
+            case 11:
+                message = "So many red zone choices, EEK!"
             default:
                 message = "Preplan next time."
             }
@@ -45,7 +52,7 @@ struct PopUpMessage: View {
             
             //Category 2:  50% in the blue zone and 50% in the red and yellow zone
             
-            let randomInt = Int.random(in: 1..<10)
+            let randomInt = Int.random(in: 1..<12)
             
             switch(randomInt){
             case 1:
@@ -57,6 +64,12 @@ struct PopUpMessage: View {
                 
             case 4:
                 message = "Clean out your kitchen cabinets and frig of poor food choices!"
+            case 5:
+                message = "So so food choices today."
+            case 6:
+                message = "Grocery shop with a list and purpose!"
+            case 7:
+                message = "Choose meals and recipes with more vegetables!"
             default:
                 message = "I will make better food choices starting with my next meal!"
             }
@@ -76,7 +89,13 @@ struct PopUpMessage: View {
             case 4:
                 message = "Remember this is a process and it will get better!"
             case 5:
-                message = "Improving my health through good food choices and exercise"
+                message = "Improving my health through good food choices and exercise!"
+            case 6:
+                message = "One good choice leads to another!"
+            case 7:
+                message = "Keep adding more foods from the blue zone."
+            case 8:
+                message = "Good sleep habits improve good food choices!"
             default:
                 message = "Think about how your energy levels are improving as you eat more anti-inflammatory foods."
                 
@@ -97,6 +116,10 @@ struct PopUpMessage: View {
                 message = "My health is the single most important goal for me. "
             case 5:
                 message = "I WILL continue this Life changing program"
+            case 6:
+                message = "Keep the fridge and pantry loaded with blue zone foods. "
+            case 7:
+                message = "What new vegetable will you try today?"
             default:
                 message = "Great food choices! These are promoting a healthy and vibrant life!"
         }
@@ -105,7 +128,7 @@ struct PopUpMessage: View {
             //message = "80%+ message"
             //Category 5 >80% in the blue zone and <20% in the red and yellow zone
             
-            let randomInt = Int.random(in: 1..<10)
+            let randomInt = Int.random(in: 1..<13)
             switch(randomInt){
             case 1:
                 message = "I am feeling better with this lifestyle change!"
@@ -115,9 +138,20 @@ struct PopUpMessage: View {
                 message = "I’m decreasing inflammation and pain with good food choices and exercise."
             case 4:
                 message = "It’s important that I stay consistent."
-                
+            case 5:
+                message = "Great choices!"
+            case 6:
+                message = "Eat the rainbow !"
+            case 7:
+                message = "I intentionally incorporate vegetables, beans, fruits, nuts, and seeds in daily meals."
+            case 8:
+                message = "Awesome!"
+            case 9:
+                message = "Right on track!"
+            case 10:
+                message = "Healthier everyday!"
             default:
-                message = "I will encourage others to make changes to improve their health"
+                message = "I will encourage others to make changes to improve their health!"
             }
             
         }
